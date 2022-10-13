b870d7143176a0261bdf36bd6c67c0dfcaae49b5
leoleq <googlonator@gmail.com>
2022-10-13 21:55:53 +0400

    Create slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,30 @@
-import {Loader, LoaderOptions} from 'google-maps';
 const test = require('./modules/test.js')
 
+var elem = document.querySelector('.carousel');
+var elemTwo = document.querySelector('.carousel-two');
 
+var flkty = new Flickity( elem, {
+	cellAlign: 'left',
+prevNextButtons: false,
+	pageDots: false,
+	wrapAround: true,
+	contain: true,
+	freeScroll: true
+});
+
+var flktyTwo = new Flickity( elemTwo, {
+	cellAlign: 'left',
+prevNextButtons: false,
+	pageDots: false,
+	wrapAround: true,
+	contain: true,
+	freeScroll: true
+});
+
+//flktyTwo.next();
+//flktyTwo.select( 2 );
+//flkty.resize()
+//flkty.reposition()
 
 const ready = () => {
   test()
